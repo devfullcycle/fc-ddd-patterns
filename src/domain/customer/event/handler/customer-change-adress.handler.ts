258d52b3e801ccdcd171156fab802ebdae3aafc4
Jennysson Júnior <jddsjr98@gmail.com>
2023-03-28 09:10:44 -0300

    fix: type of change adress handler

--- src/domain/customer/event/handler/customer-change-adress.handler.ts
+++ src/domain/customer/event/handler/customer-change-adress.handler.ts
@@ -1,9 +1,8 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import { CustomerChangeAddressEvent } from "../customer-change-adress.event";
-import { CustomerCreatedEvent } from "../customer-created.event";
 
 export class CustomerChangeAddressHandler
-  implements EventHandlerInterface<CustomerCreatedEvent>
+  implements EventHandlerInterface<CustomerChangeAddressEvent>
 {
   handle({eventData}: CustomerChangeAddressEvent): void {
     console.log(`Endereço do cliente: ${eventData.id}, ${eventData.nome} alterado para: ${eventData.endereco}`);
