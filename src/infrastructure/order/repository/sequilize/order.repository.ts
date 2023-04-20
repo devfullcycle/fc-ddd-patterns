50de6964fc6dbeacd49078e9b0853d7af325704c
blarth <jminocente@gmail.com>
2023-04-20 07:51:26 -0300

    feat: order-repo-implementation infra

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,4 +1,5 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
@@ -20,6 +21,56 @@ export default class OrderRepository {
       {
         include: [{ model: OrderItemModel }],
       }
+    );
+  }
+  async update(entity: Order): Promise<void>{
+    await OrderModel.update(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      { where: { id: entity.id } }
+    );
+  }
+  async find(id: string): Promise<Order> {
+    let orderModel;
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        rejectOnEmpty: true,
+        include : [{ model: OrderItemModel }]
+      });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+    const orderItems = orderModel.items.map((item: OrderItemModel) => {
+      return new OrderItem(
+        item.id,
+        item.name,
+        item.price,
+        item.product_id,
+        item.quantity
+        ) 
+    })
+    const order = new Order(orderModel.id, orderModel.customer_id, orderItems)
+    return order;
+  }
+  async findAll(): Promise<Order[]> {
+    const orderModels = await OrderModel.findAll();
+    return orderModels.map((orderModel) =>
+      new Order(orderModel.id, orderModel.customer_id, orderModel.items.map((orderItem) => {
+        return new OrderItem(orderItem.id, orderItem.name, orderItem.price, orderItem.product_id, orderItem.quantity)
+      }))
     );
   }
 }
